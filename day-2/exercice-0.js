35201f77bee0f4f647b1ee5ffa42a1af817293ac
eggøer <eggercedric78@gmail.com>
2021-10-20 14:51:17 +0200

    Exercice 0

---
+++ day-2/exercice-0.js
@@ -0,0 +1,16 @@
+// Créer un fichier  ˜/mylastname-my-exercices/day-2/exercice-0.js 
+// Elle devra être prototypée de la manière suivante : my_display_forever_42(nbr)
+export const my_display_forever_42 = (nbr) => {
+    
+    // Si aucun argument n’est passé en paramètre renvoyer 42.
+    // Si tout autre chose hormis un nombre est passé en paramètre renvoyer 42
+    var number = 42;
+
+    // Si un nombre est passé en argument renvoyer le nombre passé en paramètre. 
+    if (typeof(nbr) === "number")
+        return number;
+    
+    // #Indices
+    // Retour attendu : Number
+    return number;
+}
