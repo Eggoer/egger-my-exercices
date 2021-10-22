55cbfe644871199e5035d3b0613f6f89abccffd4
eggøer <eggercedric78@gmail.com>
2021-10-22 10:01:04 +0200

    Exercice 1

--- day-2/exercice-1.js
+++ day-2/exercice-1.js
@@ -1,14 +1,13 @@
 // Créer un fichier  ˜/mylastname-my-exercices/day-2/exercice-1.js 
 // Elle devra être prototypée de la manière suivante : my_alpha_reverse(str)
-import { my_size_alpha } from "../day-1/exercice-4";
+import { my_size_alpha } from "../day-1/exercice-4.js";
 
 export const my_alpha_reverse = (str) => {
     
     // Faite en sorte d’inverser les chaines de caractères passé en argument
-    var nbr = my_size_alpha(str);
     var string = "";
 
-    for (var i = nbr - 1; i > 0; i--)
+    for (var i = my_size_alpha(str) - 1; i >= 0; i--)
         string += str[i];
 
     // #Indices
@@ -17,3 +16,4 @@ export const my_alpha_reverse = (str) => {
 }
 // #Warning
 // Les push, concat, join, reverse ... et toutes les autres méthodes pour manipuler les tableaux et les string sont interdites
+console.log(my_alpha_reverse("test"));
